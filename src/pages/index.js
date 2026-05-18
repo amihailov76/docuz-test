7dc74855391221b550abe89360cd9a7a3626d1de
amihailov76 <amihailov76@gmail.com>
2026-05-18 15:28:06 +0300

    index fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import { Redirect } from '@docusaurus/router';
 
 export default function Home() {
-  return <Redirect to="docs/ru/introduction" />;
+  return <Redirect to="/docs/ru/introduction" />;
 }
